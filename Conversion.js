d71d93a50e82d5b1428073cb9c14930524da155d
MaheshHande <48543390+MaheshHande@users.noreply.github.com>
2024-05-16 18:57:29 +0530

    Add files via upload

---
+++ Conversion.js
@@ -0,0 +1,25 @@
+let score = "33"
+console.log(typeof score);
+
+// // Converting To Number 
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+let value = null;
+
+let Converting = Number(value);
+console.log(Converting);
+console.log(typeof Converting);
+
+// "33" => 33
+//  "33abc" => NaN (Not A Number)
+// true => 1 , fales => 0
+
+let isLoggedIn = 1 
+let ConTOBool = Boolean(isLoggedIn);
+console.log(ConTOBool);
+console.log(typeof ConTOBool);
+
---
+++ Datatypes.js
@@ -0,0 +1,39 @@
+
+/* JavaScript Has The This Type Of Data Types  :
+1.String
+2.Number
+3.Boolean
+4.null
+5.undefined
+
+Object
+
+*/
+
+// String
+
+let name = "Mahesh Hande"
+console.log(name);
+
+// Numbers 
+
+let age = 25
+console.log(age);
+
+// Boolean 
+
+let isAge25 = true;
+console.log(isAge25);
+
+// // Null
+// let empty =
+// console.log(empty);
+
+
+// undefined 
+
+let unknown ;
+console.log(unknown);
+
+console.log(typeof name);
+console.log(typeof null);
