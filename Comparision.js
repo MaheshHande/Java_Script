676be1040ee3ff3354d88c9a0f4eed9c5fee8893
MaheshHande <48543390+MaheshHande@users.noreply.github.com>
2024-05-17 12:44:23 +0530

    Add files via upload

---
+++ Comparision.js
@@ -0,0 +1,16 @@
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+// === is Checks The Value And DataType
+
+console.log("2"===2);
+
+
+
