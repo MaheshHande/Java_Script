e85886c8f80c23d606c9c9aa443202b0d583b294
MaheshHande <48543390+MaheshHande@users.noreply.github.com>
2024-05-18 21:11:01 +0530

    Add files via upload

---
+++ String.js
@@ -0,0 +1,53 @@
+// String in JS And Methods 
+const Name = "Mahesh Hande";
+console.log(Name);
+
+
+
+
+// String As a Object :-
+// We Can Define String Object by using new 'Key' word 
+let y = new String("Iron Man");
+
+
+
+
+
+// Methods OF String:-
+// 1.Length Function
+console.log("String Length of Mahesh: "+Name.length);
+console.log("String Length of Iron Man is: "+y.length);
+
+
+
+// 2.charAt()
+console.log(Name.charAt(2));
+console.log(y.charAt(3));
+
+
+
+// 3.charCodeAt()
+console.log(Name.charCodeAt(0));
+
+
+
+// 4.at()--> Gives The String AT the Index Position
+console.log(Name.at(2));
+
+
+
+// 5.slice()
+
+console.log(Name.slice(0,6));
+
+console.log(Name.slice(0));
+
+console.log(Name.slice(6));
+
+console.log(Name.slice(-12));
+
+
+
+
+
+
---
+++ String2.js
@@ -0,0 +1,39 @@
+
+// // Js Functions :-
+
+// const x = "      Mahesh Hande    "
+// let y =   "      Mahesh Hande    "
+
+
+
+// // Trim()
+
+// console.log(x.trim());
+// console.log(x);
+// console.log(typeof x);
+
+
+
+// // Trim White Spaces From Start use trimstart()
+
+// console.log(x.trimStart());
+
+
+// // Trim White Spaces From Start use trimstart()
+
+// console.log(x.trimEnd());
+
+
+
+// Search in String 
+
+let text = "This is The Demo Text of 'Text'"
+console.log(text.indexOf("The")); 
+console.log(text.lastIndexOf("Text"));
+let index = text.indexOf("Text", 15);
+console.log(index);
+
+// Matching String 
+let text1 = text.match("is");
+console.log(text1);
+
